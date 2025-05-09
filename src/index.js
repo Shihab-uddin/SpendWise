76dbf326895df19413a1752e41c5000a530ef750
Shihab-uddin <67788532+Shihab-uddin@users.noreply.github.com>
2025-05-10 00:21:05 +0600

    ip changes to listen to expo go app

--- src/index.js
+++ src/index.js
@@ -40,6 +40,6 @@ app.use(errorHandler);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
